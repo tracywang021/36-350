0b277364a3ea4c0c0b582072025b068609060cb2
tracyykqm <tracyykqm@gmail.com>
2020-11-13 19:05:26 -0500

    d

--- dark_and_stormy.R
+++ dark_and_stormy.R
@@ -1,2 +1,1 @@
-print("It was a dark and stormy night so I stayed in to complete my R project and contemplate the future."source_url("https://raw.githubusercontent.com/tracywang021/36-350/main/dark_and_stormy.R")
-
+print("It was a dark and stormy night so I stayed in to complete my R project and contemplate the future.")
